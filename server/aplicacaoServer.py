1db88c1c79c150dd135c50258afc62bc07250653
pedroaltobelli23 <73080145+pedroaltobelli23@users.noreply.github.com>
2022-03-17 13:36:25 -0300

    atualizcao server

--- server/aplicacaoServer.py
+++ server/aplicacaoServer.py
@@ -9,6 +9,7 @@
 #esta é a camada superior, de aplicação do seu software de comunicação serial UART.
 #para acompanhar a execução e identificar erros, construa prints ao longo do código! 
 
+import sys
 from enlaceServer import *
 import time
 import numpy as np
@@ -33,7 +34,52 @@ def main():
         # Ativa comunicacao. Inicia os threads e a comunicação seiral 
         server.enable()
         
+        #ETAPAS:
+        #HandShake
+            #recebe handshakepackage do client
+            #Envia resposta para client
+            #Responde sim para a mensagem eviada pelo client
         
+        handshakepkg,_nrx = server.getData(14)
+        print(handshakepkg)
+        server.sendData(handshakepkg)
+        print('handshake resposta enviado')
+
+        #=========RECEBENDO IMAGEM=========#
+        IMAGEM = b""
+        first_head,nRx = server.getData(10)
+        print(first_head[2])
+        n_pkg = first_head[2]
+
+        if n_pkg < 114:
+            msgandEOP,nRx = server.getData(n_pkg+4)
+            print(msgandEOP)
+        else:
+            f_payload,nRx=server.getData(n_pkg)
+            f_EOP,nRx = server.getData(4)
+            if f_EOP == b'\xEE\x23\x4C\xA9':
+                print("primeiro enviado")
+            else:
+                print("Something went wrong")
+                sys.exit()
+            
+            for i in range(n_pkg-1):
+                head,nRx = server.getData(10)
+                tamanho = head[2]
+                numero_do_pkg = head[0]
+                payload,nRx=server.getData(tamanho)
+                IMAGEM+=payload
+                EOP,nRx = server.getData(4)
+
+                if EOP != b'\xEE\x23\x4C\xA9':
+                    sys.exit()
+                 
+                print(IMAGEM)
+                server.sendData(handshakepkg)
+            
+                
+
+
 
         # Encerra comunicação
         print("-------------------------")
